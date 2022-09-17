271057b34d7f6a34b13f4e151ae55370cfd3a0db
itsuki.matsunaga <itsuki.matsunaga@koshizuka-lab.org>
2022-09-18 00:07:04 +0900

    changed dir str

--- src/trainer.py
+++ src/trainer.py
@@ -39,7 +39,7 @@ class Trainer:
         save_epoch_steps=None,
         random_seed=config.RANDOM_SEED,
     ):
-        fix_seed(random_seed)
+        # fix_seed(random_seed)
         start = time.time()
 
         for epoch in range(1, n_epochs + 1):
@@ -105,7 +105,7 @@ class Trainer:
                 data = map(lambda x: x.to(device=self.device), data)
             else:
                 data = data.to(device=self.device)
-            out = self.model(data)
+            out = self.model.generate(data)
 
         return out
 
@@ -126,9 +126,12 @@ class Trainer:
             else:
                 data = data.to(device=self.device)
             target = target.to(device=self.device)
+            
+            decoder_xs = torch.full_like(target, -1)
+            decoder_xs[..., 1:] = target[..., :-1]
 
-            out = self.model(data)
-            loss = self.loss_fn(out, target)
+            out = self.model(data, decoder_xs)
+            loss = self.loss_fn(out, target.view(target.shape[0], -1))
 
             self.optimizer.zero_grad()
             loss.backward()
@@ -150,8 +153,8 @@ class Trainer:
                     data = data.to(device=self.device)
                 target = target.to(device=self.device)
 
-                out = self.model(data)
-                loss = self.loss_fn(out, target)
+                predicted = self.model.generate(data)
+                loss = self.loss_fn(predicted, target.view(target.shape[0], -1))
 
                 valid_loss += loss.item()
 
